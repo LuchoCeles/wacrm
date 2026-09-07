3c2c32ffe5eb3fd4008c950394fc371963db327c
Luciano Celes <lucianoceles@gmail.com>
2026-09-07 17:51:39 -0300

    fix: Titulo de la app modificada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,8 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: {
-    default: "wacrm",
-    template: "%s — wacrm",
+    default: "Logabyte WhatsApp Business CRM",
+    template: "%s — Logabyte WhatsApp Business CRM",
   },
   description: "Self-hostable CRM template for WhatsApp.",
   robots: {
